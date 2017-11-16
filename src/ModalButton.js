002e9c9740c1a4dcbfe9ba242a55b3cf873ab10c
TAksenov <taksenov@gmail.com>
2017-11-16 21:26:11 +0500

    ModalButton tests -- all done

--- src/ModalButton.js
+++ src/ModalButton.js
@@ -1,9 +1,36 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
+import Modal from './Modal';
 
 class ModalButton extends Component {
-  render() {
-    return null;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalShow: false
+        };
+        this.hideModal = this.hideModal.bind(this);
+        this.showModal = this.showModal.bind(this);
+        this.renderModal = this.renderModal.bind(this);
+    } // constructor
+
+    hideModal() {
+        this.setState({
+            isModalShow: false
+        });
+    } //hideModal
+
+    showModal() {
+        this.setState({
+            isModalShow: true
+        });
+    } //showModal
+
+    renderModal() {
+        if (this.state.isModalShow === true) return <Modal />;
+    } //renderModal
+
+    render() {
+        return <div>{this.renderModal()}</div>;
+    }
 }
 
 export default ModalButton;
