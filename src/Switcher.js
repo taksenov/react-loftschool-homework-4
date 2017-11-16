70054d8e7e1fe142f0a4d1348e8fb63ba678a893
TAksenov <taksenov@gmail.com>
2017-11-16 15:45:57 +0500

    add handler for Switcher

--- src/Switcher.js
+++ src/Switcher.js
@@ -1,13 +1,24 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import './Switcher.css';
 
 // Для работы этой компоненты нужно использовать методы React.Children.toArray
 // а так же работать с child.type.name и child.type.displayName
 
 class Switcher extends Component {
-  render() {
-    return null;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedChild: 0
+        };
+
+        this.handleChangeChild = this.handleChangeChild.bind(this);
+    }
+
+    handleChangeChild() {} //handleChangeChild
+
+    render() {
+        return null;
+    }
 }
 
 export default Switcher;
